762c7acb087d0a6449cfa8c8f4d854986f92e825
AppleIpx <zhenya.0295@mail.ru>
2023-10-12 23:38:12 +0300

    Добавлен разрешенный хост

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,6 +8,6 @@ python3 manage.py migrate --no-input
 
 python3 manage.py collectstatic --no-input
 
-#python3 manage.py filling_db
+python3 manage.py filling_db
 
 gunicorn foodgram.wsgi:application --bind 0.0.0.0:8000
