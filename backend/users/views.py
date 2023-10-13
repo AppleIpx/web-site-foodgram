9a18093f106d83fcbe20c40415461dcbc6fa904c
AppleIpx <zhenya.0295@mail.ru>
2023-10-13 16:41:35 +0300

    Исправлена ошибка с добавлением пароля пользователя в бд

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from django.shortcuts import get_object_or_404
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
@@ -19,6 +20,13 @@ class UserView(viewsets.ModelViewSet):
         user = get_object_or_404(User, pk=request.user.id)
         serializer = UserSerializer(user)
         return Response(serializer.data)
+
+    def perform_create(self, serializer):
+        if "password" in self.request.data:
+            password = make_password(self.request.data["password"])
+            serializer.save(password=password)
+        else:
+            serializer.save()
 
     @action(["post"], detail=False, permission_classes=(IsAuthenticated, ))
     def set_password(self, request):
