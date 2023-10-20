e8a32396aae6936e339019501f63bcb1f201b8c9
AppleIpx <zhenya.0295@mail.ru>
2023-10-20 16:40:46 +0300

    Добавлена документация swagger

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('SECRET_KEY')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -50,7 +49,7 @@ INSTALLED_APPS = [
     'django_filters',
     'users',
     'web_site',
-
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -95,13 +94,16 @@ DATABASES = {
     }
 }
 
+SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': False
+}
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    "DEFAULT_PAGINATION_CLASS": [
+    "DEFAULT_PAGINATION_CLASS":
         "rest_framework.pagination.PageNumberPagination",
-    ],
     "PAGE_SIZE": 6,
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend"
